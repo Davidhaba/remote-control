2f88d4d87a93b41d71237bd57dd9987aa8bf7f38
David <154765655+Davidhaba@users.noreply.github.com>
2026-07-29 08:04:13 +0200

    Add files via upload

--- web_server.py
+++ web_server.py
@@ -10,12 +10,7 @@ from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your-secret-key'
-try:
-    import eventlet
-    async_mode = 'eventlet'
-except Exception:
-    async_mode = 'threading'
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode=async_mode)
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='threading')
 
 registered_servers = {}
 browser_sessions = {}
